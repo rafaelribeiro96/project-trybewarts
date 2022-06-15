06d974911079e1259d9f60756ae7f3fce9c8de3c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-06-15 16:14:38 -0300

    requisito 18

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ const start = () => {
   loginBtn.addEventListener('click', validateLogin);
 };
 start();
+
+function checkAgreement() {
+  const checkbox = document.getElementById('agreement');
+  const sendButton = document.getElementById('submit-btn');
+
+  if (checkbox.checked) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
+}
+
+const checkbox = document.getElementById('agreement');
+checkbox.onchange = checkAgreement;
